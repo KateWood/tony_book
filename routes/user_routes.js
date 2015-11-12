121082aa01ad0a230434ad5a4b06e4e1d62e8f83
Kate Wood <katewood611@gmail.com>
2015-11-12 12:13:18 -0800

    adds post for /users route using the create action from the users controller

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,8 +2,9 @@ var usersController = require('../controllers/users_controller.js'),
 	express			= require('express'),
 	userRoutes		= express.Router() 
 
-//create the index route for all users
+//create routes for /users
 userRoutes.route('/')
 	.get(usersController.index)
+	.post(usersController.create)
 
 module.exports = userRoutes
