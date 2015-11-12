0ceca4b0da726db875bb9f4176fc3fe4f1b0bbe3
Kate Wood <katewood611@gmail.com>
2015-11-12 11:31:22 -0800

    adds user index route

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,3 +1,9 @@
 var usersController = require('../controllers/users_controller.js'),
 	express			= require('express'),
 	userRoutes		= express.Router() 
+
+//create the index route for all users
+userRoutes.get('/', function(req, res) {
+	//user the controller to retrieve all the users
+	res.json(usersController.index())
+})
