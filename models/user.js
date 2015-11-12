0bdc0a08e66f4ecaa2477b4e4fcb254171c91352
Kate Wood <katewood611@gmail.com>
2015-11-12 10:32:33 -0800

    creates userSchema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,9 @@
 var mongoose = require('mongoose')
+
+//create user schema
+var userSchema = new mongoose.Schema({
+	user_name: String,
+	email: {type: String, required: true, unique: true},
+	friends: [],
+	age: Number
+})
