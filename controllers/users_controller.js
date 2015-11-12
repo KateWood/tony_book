40ba96cb46b485071e5a28b916868ac4b9ff48ca
Kate Wood <katewood611@gmail.com>
2015-11-12 12:07:27 -0800

    adds create action to users controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,29 @@ function index(req, res) {
 	})
 }
 
+//create action to add a new user
+function create(req,res){
+  // make a single user -- create
+	console.log("Creating a user")
+	var user = new User()
+​
+	user.name = req.body.name
+	user.username = req.body.username
+	user.password = req.body.password
+​
+	user.save(function(err){
+		if(err){
+			if(err.code == 11000){
+				return res.json({success: false, message: "username already exists" })
+			} else {
+				res.send(err)
+			}
+		}
+		res.json({success: true, message: "User created, Wahey!"})
+	})
+}
+
 module.exports = {
-	index: index
+	index: index,
+	create: create
 }
