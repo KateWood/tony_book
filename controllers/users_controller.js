c8dfb8d28d88aad1a9bd717bb6dd4f6683ceaab7
Kate Wood <katewood611@gmail.com>
2015-11-12 10:58:52 -0800

    adds index action to users controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,1 +1,9 @@
 var User = require('../models/user.js')
+
+//create index action to display all users
+module.exports.index = function() {
+	User.find({}, function(err, users) {
+		if (err) console.log(err)
+		return users
+	})
+}
