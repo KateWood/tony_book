2633afcce51a7884c69a646891447cce19a9e22d
Kate Wood <katewood611@gmail.com>
2015-11-12 12:12:02 -0800

    refactors user create action for style

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,24 +9,23 @@ function index(req, res) {
 }
 
 //create action to add a new user
-function create(req,res){
-  // make a single user -- create
-	console.log("Creating a user")
+function create(req, res) {
+	// make a single user -- create
+	console.log('Creating a user')
 	var user = new User()
-​
-	user.name = req.body.name
-	user.username = req.body.username
-	user.password = req.body.password
-​
+
+	user.user_name = req.body.user_name
+	user.email = req.body.email
+	
 	user.save(function(err){
 		if(err){
 			if(err.code == 11000){
-				return res.json({success: false, message: "username already exists" })
+				return res.json({success: false, message: 'username already exists' })
 			} else {
 				res.send(err)
 			}
 		}
-		res.json({success: true, message: "User created, Wahey!"})
+		res.json({success: true, message: 'User created, Wahey!'})
 	})
 }
 
